57c5655910105ee236659eb1fe79fa89104191cd
Andrej-ori <a31338805@gmail.com>
2022-07-14 18:50:57 +0300

    modification commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int invoiceAmount = 365; // Сумма на счете клиента
-        int replenishmentAmount = 4876; // Сумма пополнения счета клиента
+        System.out.println();
+        System.out.println("Приложение для расчета суммы бонусных рублей при пополнении счета болле чем на 1 000 рублей");
+        System.out.println();
+
+        Scanner num = new Scanner(System.in);
+        int invoiceAmount; // Сумма на счете клиента
+        int replenishmentAmount; // Сумма пополнения счета клиента
         int bonusAmount = 1; // Сумма бонуса
         int bonus; // количество начисленных бонусов
 
+        System.out.println("Введите первоночальную сумму на счете:");
+        invoiceAmount = num.nextInt();
+
+        System.out.println("Введите сумму пополнения счета:");
+        replenishmentAmount = num.nextInt();
+
         System.out.println("Начальные данные: у клиента на счёте " + invoiceAmount + " рублей(я)."); // Вывод начальной суммы на счете клиента
 
-        if (replenishmentAmount >= 1_000) { // проверка выопления условия для начисления бонуса
+         if (replenishmentAmount > 1_000) { // проверка выопления условия для начисления бонуса
             bonus = replenishmentAmount / 100 * bonusAmount; // расчет суммы бонуса
-        }
-        else { // условие для начисления бонуса не выполнено
-            bonus = 0; // бонус равен нулю
-        }
-        invoiceAmount = invoiceAmount + replenishmentAmount + bonus; // расчет итоговой суммы после пополнения счета и начисления бонусных рублей
+         }
+         else { bonus = 0; // бонус равен нулю
+         }
+         invoiceAmount = invoiceAmount + replenishmentAmount + bonus; // расчет итоговой суммы после пополнения счета и начисления бонусных рублей
 
-        if (bonus == 0) {
+         if (bonus == 0) {
             System.out.println("Клиент пополнил счёт на " + replenishmentAmount + " рублей(я) - бонусов нет, итоговая сумма на счету клиента - " + invoiceAmount + " рублей(я).");
-        }
-        else {
-            System.out.println("Клиент пополнил счёт на " + replenishmentAmount + " рублей(я) - бонус равен " + bonus + " рублям(ю), итоговая сумма на счету клиента - " + invoiceAmount + " рублей(я).");
-        }
+         }
+         else { System.out.println("Клиент пополнил счёт на " + replenishmentAmount + " рублей(я) - бонус равен " + bonus + " рублям(ю), итоговая сумма на счету клиента - " + invoiceAmount + " рублей(я).");
+         }
         // вывод суммы пополнения, итоговой суммы и количества бонусов
 
     }
