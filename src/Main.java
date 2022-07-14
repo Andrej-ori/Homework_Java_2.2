3aaa04829c6582a0436a411f0287b61dd2bda3e0
Andrej-ori <107578974+Andrej-ori@users.noreply.github.com>
2022-07-14 19:01:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,23 @@ public class Main {
         System.out.println();
         System.out.println("Приложение для расчета суммы бонусных рублей при пополнении счета болле чем на 1 000 рублей");
         System.out.println();
-
+        // Вывод описания приложения
+        
         Scanner num = new Scanner(System.in);
         int invoiceAmount; // Сумма на счете клиента
         int replenishmentAmount; // Сумма пополнения счета клиента
         int bonusAmount = 1; // Сумма бонуса
         int bonus; // количество начисленных бонусов
-
+        //Обьявление переменых        
+        
         System.out.println("Введите первоночальную сумму на счете:");
         invoiceAmount = num.nextInt();
-
+        //оператор для ручного ввода данных (сумма на счете клиента)       
+        
         System.out.println("Введите сумму пополнения счета:");
         replenishmentAmount = num.nextInt();
-
+        //оператор для ручного ввода данных (сумма пополнения счета)
+        
         System.out.println("Начальные данные: у клиента на счёте " + invoiceAmount + " рублей(я)."); // Вывод начальной суммы на счете клиента
 
          if (replenishmentAmount > 1_000) { // проверка выопления условия для начисления бонуса
